39d97e536c873fe928894bdfa25d9382f0b22c87
fuwillie <fuwillie@us.ibm.com>
2016-10-19 14:03:15 -0700

    L37

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe';
+
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: 'recipe-list.component.html'
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  recipe = new Recipe('Dummy', 'Dummy', 'http://placekitten.com/50/50');
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
